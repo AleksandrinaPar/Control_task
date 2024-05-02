b026bf6b03d7d94b2edd89c2338b4402d6576c8d
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-05-02 07:44:19 +0300

    Update Program.cs
    Исправил, что указал препод.

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,48 +8,74 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
+using System;
 
-// Шаг 1: Запрос размера массива строк у пользователя
-Console.Write("Введите количество строк в массиве: ");
-int arraySize = Convert.ToInt32(Console.ReadLine());
-
-// Шаг 2: Ввод данных пользователем и сохранение в массиве
-string[] inputStrings = new string[arraySize];
-for (int i = 0; i < arraySize; i++)
+class Program
 {
-    Console.Write($"Введите строку {i + 1}: ");
-    inputStrings[i] = Console.ReadLine();
-}
-
-// Шаг 3: Подсчет подходящих строк (длина не более 3 символов)
-int suitableCount = 0;
-foreach (string str in inputStrings)
-{
-    if (str.Length <= 3)
-        suitableCount++;
-}
-
-// Шаг 4: Создание нового массива с подходящими строками
-string[] suitableStrings = new string[suitableCount];
-int index = 0;
-foreach (string str in inputStrings)
-{
-    if (str.Length <= 3)
+    static void Main()
     {
-        suitableStrings[index] = str;
-        index++;
+        int arraySize = RequestArraySize();
+        string[] inputStrings = ReadInputStrings(arraySize);
+        string[] suitableStrings = FilterStrings(inputStrings);
+        DisplayResults(inputStrings, suitableStrings);
     }
-}
 
-// Шаг 5: Вывод результатов
-Console.WriteLine("\nИсходный массив строк:");
-foreach (string str in inputStrings)
-{
-    Console.WriteLine(str);
-}
+    static int RequestArraySize()
+    {
+        Console.Write("Введите количество строк в массиве: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
 
-Console.WriteLine("\nНовый массив с подходящими строками (длина <= 3 символов):");
-foreach (string str in suitableStrings)
-{
-    Console.WriteLine(str);
+    static string[] ReadInputStrings(int arraySize)
+    {
+        string[] strings = new string[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            Console.Write($"Введите строку {i + 1}: ");
+            strings[i] = Console.ReadLine();
+        }
+        return strings;
+    }
+
+    static string[] FilterStrings(string[] inputStrings)
+    {
+        int suitableCount = CountSuitableStrings(inputStrings);
+        string[] suitableStrings = new string[suitableCount];
+        int index = 0;
+        foreach (string str in inputStrings)
+        {
+            if (str.Length <= 3)
+            {
+                suitableStrings[index] = str;
+                index++;
+            }
+        }
+        return suitableStrings;
+    }
+
+    static int CountSuitableStrings(string[] strings)
+    {
+        int count = 0;
+        foreach (string str in strings)
+        {
+            if (str.Length <= 3)
+                count++;
+        }
+        return count;
+    }
+
+    static void DisplayResults(string[] inputStrings, string[] suitableStrings)
+    {
+        Console.WriteLine("nИсходный массив строк:");
+        foreach (string str in inputStrings)
+        {
+            Console.WriteLine(str);
+        }
+
+        Console.WriteLine("nНовый массив с подходящими строками (длина <= 3 символов):");
+        foreach (string str in suitableStrings)
+        {
+            Console.WriteLine(str);
+        }
+    }
 }
